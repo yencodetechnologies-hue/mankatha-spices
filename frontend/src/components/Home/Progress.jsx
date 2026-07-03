282f0b166e85fae4b9adbb4afa98980febf6067d
Irin-07 <irinamalfelshiyaa@gmail.com>
2026-07-03 16:34:00 +0530

    ui

--- frontend/src/components/Home/Progress.jsx
+++ frontend/src/components/Home/Progress.jsx
@@ -13,7 +13,7 @@ import {
 const Progress = () => {
 	const navigate = useNavigate();
     const handleDonateClick = () => {
-        navigate("/about");
+        navigate("/Care/about");
     };
 	const createSectionStyle = (imageUrl) => ({
 		backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.5)),url(${imageUrl})`,
